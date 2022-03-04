9e0c82092e70091d3e5fd2c2da51d59752e0da1f
Ravi <ravigyansingh@gmail.com>
2022-03-04 14:40:35 +0530

    t

--- src/main/java/com/jlcindia/springboot/dao/BookDAO.java
+++
@@ -1,8 +0,0 @@
-package com.jlcindia.springboot.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.jlcindia.springboot.entity.Book;
-
-public interface BookDAO extends JpaRepository<Book, Integer> {
-
-}
