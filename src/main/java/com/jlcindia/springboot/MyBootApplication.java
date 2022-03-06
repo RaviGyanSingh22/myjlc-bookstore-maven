17c94f6dcb4b8b343beddbd6c975919e13e27f85
RaviGyanSingh22 <96980872+RaviGyanSingh22@users.noreply.github.com>
2022-03-06 06:21:17 +0530

    Update MyBootApplication.java

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyBootApplication {
 	public static void main(String as[]) {
+		System.out.println("Update1 ");
 		SpringApplication.run(MyBootApplication.class, as);
 	}
 }
