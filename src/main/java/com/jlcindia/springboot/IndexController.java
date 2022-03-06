f35e89e0a9aa29c4d882f050ab29b7a2c97e4e3b
Ravi <ravigyansingh@gmail.com>
2022-03-06 12:48:16 +0530

    t

--- src/main/java/com/jlcindia/springboot/IndexController.java
+++ src/main/java/com/jlcindia/springboot/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 	@GetMapping("/")
 	public String showIndexPage() {
-		System.out.println("---------showIndexPage---------");
+		System.out.println("---------showIndexPage 1---------");
 		return "index";
 	}
 }
