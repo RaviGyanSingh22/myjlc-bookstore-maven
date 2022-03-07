9ceafcc98191d2f7f03b4904ccada628fd05e536
Ravi <ravigyansingh@gmail.com>
2022-03-07 13:56:27 +0530

    t

--- src/main/java/com/jlcindia/springboot/IndexController.java
+++ src/main/java/com/jlcindia/springboot/IndexController.java
@@ -1,13 +1,31 @@
 package com.jlcindia.springboot;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class IndexController {
-	@GetMapping("/")
-	public String showIndexPage() {
+	@GetMapping("/test")
+	public List<BookInfo> showIndexPage() {
 		System.out.println("---------showIndexPage 4---------");
-		return "index";
+
+		List<BookInfo> blist = new ArrayList<>();
+
+		BookInfo bookInfo = new BookInfo();
+		bookInfo.setAuthor("Ravi");
+		bookInfo.setBid(102);
+		bookInfo.setBname("java");
+		bookInfo.setPrice(new BigDecimal(600));
+		bookInfo.setPub("test");
+
+		// model.addAttribute("MyBooksList", blist);
+		blist.add(bookInfo);
+		return blist;
 	}
 }
--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -2,17 +2,10 @@ package com.jlcindia.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyBootApplication extends SpringBootServletInitializer {
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(MyBootApplication.class);
-	}
-
-	public static void main(String as[]) {
+public class MyBootApplication {
+		public static void main(String as[]) {
 		SpringApplication.run(MyBootApplication.class, as);
 	}
 }
