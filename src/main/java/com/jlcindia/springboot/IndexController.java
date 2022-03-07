ce798201ae2c7a4ec53b10c0e33a219f89159efb
Ravi <ravigyansingh@gmail.com>
2022-03-07 15:32:13 +0530

    t

--- src/main/java/com/jlcindia/springboot/IndexController.java
+++ src/main/java/com/jlcindia/springboot/IndexController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 	@GetMapping("/test")
 	public List<BookInfo> showIndexPage() {
-		System.out.println("---------showIndexPage 4---------");
+		System.out.println("---------showIndexPage 5---------");
 
 		List<BookInfo> blist = new ArrayList<>();
 
